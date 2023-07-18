86075a74a4729338a82a0edb754401c45b47cb7e
nivalf <flavinleejohn@gmail.com>
2023-07-18 15:22:35 +0100

    Logout added

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -7,13 +7,13 @@ import {
   import { db } from "./db.server";
   
   type LoginForm = {
-    username: string;
     password: string;
+    username: string;
   };
   
   export async function login({
-    username,
     password,
+    username,
   }: LoginForm) {
     const user = await db.user.findUnique({
       where: { username },
@@ -21,6 +21,7 @@ import {
     if (!user) {
       return null;
     }
+  
     const isCorrectPassword = await bcrypt.compare(
       password,
       user.passwordHash
@@ -78,6 +79,33 @@ import {
       throw redirect(`/login?${searchParams}`);
     }
     return userId;
+  }
+  
+  export async function getUser(request: Request) {
+    const userId = await getUserId(request);
+    if (typeof userId !== "string") {
+      return null;
+    }
+  
+    const user = await db.user.findUnique({
+      select: { id: true, username: true },
+      where: { id: userId },
+    });
+  
+    if (!user) {
+      throw logout(request);
+    }
+  
+    return user;
+  }
+  
+  export async function logout(request: Request) {
+    const session = await getUserSession(request);
+    return redirect("/login", {
+      headers: {
+        "Set-Cookie": await storage.destroySession(session),
+      },
+    });
   }
   
   export async function createUserSession(
